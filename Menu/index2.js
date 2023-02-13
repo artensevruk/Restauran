3a0e2fa7eb844b7b7a75bd7445972d66a84ddb28
Artem Sevruk <96494811+artensevruk@users.noreply.github.com>
2023-02-14 01:01:13 +0300

    Add files via upload

---
+++ Menu/index2.js
@@ -0,0 +1,4 @@
+function OpenMenu() {
+    document.getElementById("Menu").classList.toggle("open")
+}
+window.onload =OpenMenu;
---
+++ index.js
@@ -0,0 +1,7 @@
+function openPage() {
+    document.getElementById("Part2").classList.toggle("active");
+}
+window.onload =openPage;
+function OpenMenu() {
+    document.getElementById("Menu").classList.toggle("open")
+}
